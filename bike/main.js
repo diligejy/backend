7022fc6df9910302c2df80434004aeb09fc3385a
diligejy <sjy049@gmail.com>
2022-11-12 21:55:36 +0900

    API

---
+++ bike/main.js
@@ -0,0 +1,49 @@
+const API_KEY = ''
+
+async function getData(){
+    const url = `http://apis.data.go.kr/B552061/frequentzoneBicycle/getRestFrequentzoneBicycle?`
+    let params = `ServiceKey=${API_KEY}&searchYearCd=2015&siDo=11&guGun=680&type=json&numOfRows=10&pageNo=1`
+    let req = url + params
+    const res = await fetch(req);
+    const data = await res.json();
+    console.log("data", data);
+
+    const locations = data.items.item.map(spot=>[spot.spot_nm, spot.la_crd, spot.lo_crd])
+
+    console.log("locations", locations);
+
+    drawMap(locations);
+}
+
+function drawMap(locations){
+    // locations = [["지역이름", 위도, 경도],
+    //              ["지역이름", 위도, 경도]]
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom:13,
+        center: new google.maps.LatLng(locations[0][1], locations[0][2]),
+        mapTypeId : google.maps.MapTypeId.ROADMAP,
+    });
+
+    const infowindow = new google.maps.InfoWindow();
+
+    let marker, i;
+
+    for (i=0; i< locations.length; i++){
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map : map,
+        });
+
+        google.maps.event.addListener(
+            marekr,
+            "click",
+            (function (marker, i){
+                return function (){
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                };
+            })(marker, i)
+        );
+    }
+};
